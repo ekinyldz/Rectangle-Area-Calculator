0e8749961054c89bafcf828c308ee5c85b70db5b
Ekin Yildiz <115420551+ekinyldz@users.noreply.github.com>
2023-12-12 00:26:31 +0100

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Rectangle Area Calculator");
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+
+        System.out.print("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+
+        double area = calculateRectangleArea(length, width);
+
+        System.out.println("The area of the rectangle is: " + area);
+
+        scanner.close();
+    }
+
+    public static double calculateRectangleArea(double length, double width) {
+        return length * width;
+    }
+}
